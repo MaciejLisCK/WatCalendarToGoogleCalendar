1b8b2ef464595d279019a94a9edfc5a0d43e93da
Maciej Lis <mail@maciejlis.com>
2016-11-14 12:43:05 +0100

    Add project files.

---
+++ WatCalendarToGoogleCalendar/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace WatCalendarToGoogleCalendar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uxOutput = new System.Windows.Forms.TextBox();
+            this.uxInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // uxOutput
+            // 
+            this.uxOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.uxOutput.Location = new System.Drawing.Point(292, 12);
+            this.uxOutput.Multiline = true;
+            this.uxOutput.Name = "uxOutput";
+            this.uxOutput.Size = new System.Drawing.Size(272, 261);
+            this.uxOutput.TabIndex = 1;
+            // 
+            // uxInput
+            // 
+            this.uxInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.uxInput.Location = new System.Drawing.Point(12, 12);
+            this.uxInput.Multiline = true;
+            this.uxInput.Name = "uxInput";
+            this.uxInput.Size = new System.Drawing.Size(272, 261);
+            this.uxInput.TabIndex = 2;
+            this.uxInput.TextChanged += new System.EventHandler(this.uxInput_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 285);
+            this.Controls.Add(this.uxInput);
+            this.Controls.Add(this.uxOutput);
+            this.Name = "Form1";
+            this.Text = "WatCalendarToGoogleCalendar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox uxOutput;
+        private System.Windows.Forms.TextBox uxInput;
+    }
+}
+
---
+++ WatCalendarToGoogleCalendar/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WatCalendarToGoogleCalendar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void uxInput_TextChanged(object sender, EventArgs e)
+        {
+            var dataLines = uxInput.Lines.Skip(1).Take(uxInput.Lines.Length-2);
+
+            var result = "";
+            foreach (var line in dataLines)
+            {
+                var values = line.Split(',');
+
+                var courseNameShortcut = GetCourseShortcut(values[0]);
+                var labNumber = GetLabNumber(values[0]);
+                var groups = GetGroups(values[0]);
+                var location = values[1];
+                var startDateTime = GetDateTime(values[2], values[3]);
+                var endDateTime = GetDateTime(values[4], values[5]);
+                var mainLocation = "Wojskowa Akademia Techniczna im. Jarosława Dąbrowskiego, gen. Sylwestra Kaliskiego, Warszawa, Polska";
+
+                result += $"{courseNameShortcut} [{labNumber}] [{location}] {groups}\t{startDateTime}\t{endDateTime}\t\t{mainLocation}\r\n";
+            }
+            uxOutput.Text = result;
+        }
+
+        private string GetDateTime(string date, string time)
+        {
+            var dateValues = date.Split('-');
+
+            return dateValues[2] + "/" + dateValues[1] + "/" + dateValues[0] + " " + time + ":00";
+        }
+
+        private object GetGroups(string value)
+        {
+            return value.SkipWhile(c => c != ']').Skip(1).AsString().Trim();
+        }
+
+        private string GetLabNumber(string value)
+        {
+            return value.SkipWhile(c => c != '[').Skip(1).TakeWhile(c => c != ']').AsString();
+        }
+
+        private object GetCourseShortcut(string value)
+        {
+            var courseName = value.TakeWhile(v => v != '(').AsString().Trim();
+            var courseNameValues = courseName.Split(' ');
+            var courseInitials = courseNameValues.Select(cnv => cnv.First().ToUpper());
+            return courseInitials.AsString();
+        }
+
+
+
+        static string UppercaseFirst(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return string.Empty;
+            }
+            char[] a = s.ToCharArray();
+            a[0] = char.ToUpper(a[0]);
+            return new string(a);
+        }
+    }
+
+    public static class MyClass
+    {
+        public static char ToUpper(this char character)
+        {
+            return char.ToUpper(character);
+        }
+
+        public static string AsString(this IEnumerable<char> charSequence)
+        {
+            return new String(charSequence.ToArray());
+        }
+    }
+}
